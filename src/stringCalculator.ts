d94373d6efe4826dfd332f467ea7f1aba6a3b523
JayateerthDambal <jayateerth.dambal@gmail.com>
2025-01-25 01:32:41 +0530

    FEAT: Parses custom delimeter  (passing)

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,19 @@
 export function add(numbers: string): number {
     if (numbers === '') return 0;
 
-    return numbers.split(/[,\n]/)
+    // Default delimiter (comma or newline)
+    let delimiter: string | RegExp = /[,\n]/;
+    let numbersPart = numbers;
+
+    // Check: custom delimiter syntax: "//[delimiter]\n..."
+    if (numbers.startsWith('//')) {
+        const delimiterEnd = numbers.indexOf('\n');
+        delimiter = numbers.substring(2, delimiterEnd); // Extracts custom delimiter
+        numbersPart = numbers.substring(delimiterEnd + 1); // Gets numbers part
+    }
+
+    // Spilts using delimeter and returns the sum of all the numbers
+    return numbersPart.split(delimiter)
         .map(num => parseInt(num, 10))
         .reduce((sum, num) => sum + num, 0);
 }
