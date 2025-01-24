df940cb27b264491de8851eafbfcc1030448f0f3
JayateerthDambal <jayateerth.dambal@gmail.com>
2025-01-25 01:21:02 +0530

    TEST: Sum of Two Comma Separated Numbers (failing)

--- src/__tests__/stringCalculator.test.ts
+++ src/__tests__/stringCalculator.test.ts
@@ -9,8 +9,11 @@ describe('add', () => {
     // * This test is for checking Single Number 
     it('returns number itself for a single input', () => {
         expect(add('9')).toBe(9) // Test for INPUT: '5' returning 5 
-    })
+    });
 
-
+    //* This test is for Two Numbers
+    it('returns sum of two comma separated numbers', () => {
+        expect(add('4, 5')).toBe(9); // Adding 4 + 5 => 9
+    });
 
 });
