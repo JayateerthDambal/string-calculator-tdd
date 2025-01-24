179c3907f9037d0844153fa7dbe5190c16ab6270
JayateerthDambal <jayateerth.dambal@gmail.com>
2025-01-25 01:35:11 +0530

    refactor: function for extracting delimeter parsing logic

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,18 +1,20 @@
+
+// INFO: This function is a refactor for parsing custom delimeters
+function parseDelimiter(input: string): { delimiter: string | RegExp; numbers: string } {
+    if (input.startsWith('//')) {
+        const delimiterEnd = input.indexOf('\n');
+        return {
+            delimiter: input.substring(2, delimiterEnd),
+            numbers: input.substring(delimiterEnd + 1),
+        };
+    }
+    return { delimiter: /[,\n]/, numbers: input };
+}
+
 export function add(numbers: string): number {
     if (numbers === '') return 0;
 
-    // Default delimiter (comma or newline)
-    let delimiter: string | RegExp = /[,\n]/;
-    let numbersPart = numbers;
-
-    // Check: custom delimiter syntax: "//[delimiter]\n..."
-    if (numbers.startsWith('//')) {
-        const delimiterEnd = numbers.indexOf('\n');
-        delimiter = numbers.substring(2, delimiterEnd); // Extracts custom delimiter
-        numbersPart = numbers.substring(delimiterEnd + 1); // Gets numbers part
-    }
-
-    // Spilts using delimeter and returns the sum of all the numbers
+    const { delimiter, numbers: numbersPart } = parseDelimiter(numbers);
     return numbersPart.split(delimiter)
         .map(num => parseInt(num, 10))
         .reduce((sum, num) => sum + num, 0);
